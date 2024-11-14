f6a4591608d1f3dc227e1a590aec472f830612af
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2024-11-14 20:41:30 -0300

    Comentários vs Documentação

---
+++ javascript/comentarios-vs-documentacao.ts
@@ -0,0 +1,11 @@
+// Comentários vs Documentação
+
+/**
+ * Esse código foi escrito assim porque a biblioteca X ainda não suporta Y
+ *
+ * https://github.com/author/X/issues/123123
+ */
+
+// Código
+
+// Comentário não é documentação
