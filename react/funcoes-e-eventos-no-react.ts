7692018e788e5702fb7f3111f235d84aac262e67
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2024-11-14 20:47:05 -0300

    Funções e eventos no React

---
+++ react/funcoes-e-eventos-no-react.ts
@@ -0,0 +1,13 @@
+// Funções e eventos no React
+
+// sem prefixo - funções que são auxiliares de cálculo, operação, chamada de API
+
+function calculateDiscount
+
+// com prefixo handle - funções criadas dentro do componente e que são disparadas por um evento do usuário como onClick, onSubmit, etc.
+
+function handleCreateNewTodo
+
+// com prefixo on - funções passadas como parâmetro para um componente
+
+export function Header({ onCreateNewTodo})
