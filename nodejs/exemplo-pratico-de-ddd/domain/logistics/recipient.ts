99f1a5ff44f6c40bda0d494ce2c335bcb2c50600
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2024-11-21 20:01:14 -0300

    Exemplo prático de DDD

---
+++ nodejs/exemplo-pratico-de-ddd/domain/logistics/recipient.ts
@@ -0,0 +1,11 @@
+export class Recipient {
+  public street: string;
+  public number: number;
+  public zipcode: string;
+
+  constructor(street: string, number: number, zipcode: string) {
+    this.street = street;
+    this.number = number;
+    this.zipcode = zipcode;
+  }
+}
